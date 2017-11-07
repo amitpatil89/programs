1b98a89c616b200abdb5209e98997b36faebb5eb
amitpatil89 <amitpatil31dec@gmail.com>
2017-11-07 00:33:09 -0800

    Perfect Square

--- c++/perfect_rectangle.cpp
+++ c++/perfect_rectangle.cpp
@@ -133,12 +133,12 @@ class Solution {
 };
 
 int main(){
-  vector<vector<int>> rectangles {{1,1,3,3}, {3,1,4,2}, {3,2,4,4}, {1,3,2,4},{2,3,3,4}};
+  vector<vector<int>> rectangles {{1,1,3,3}, {3,1,4,2}, {3,2,4,4}, {1,3,2,4},{2,3,3,4}}; // expected output : true
 
   Solution S;
   cout << ((S.isRectangleCover(rectangles)) ? "true":"false") << endl;
 
-  rectangles = {{0,0,3,3}, {1,1,2,2}, {1,1,2,2}};
+  rectangles = {{0,0,3,3}, {1,1,2,2}, {1,1,2,2}}; // expected output : false (due to reason mentioned in step 6)
   cout << ((S.isRectangleCover(rectangles)) ? "true":"false") << endl;
 
 }
